843f1a5688669ee146b60ca77b028336895c7065
dilrajkadavithara <dilrajkadavithara@gmail.com>
2025-06-25 15:52:03 +0530

    Add sitemap URL configuration and update settings.py

--- JoseCarCareBackend/urls.py
+++ JoseCarCareBackend/urls.py
@@ -4,10 +4,13 @@ from django.contrib import admin
 from django.urls import path, include  # <-- NEW: Import include
 from django.conf import settings  # <-- NEW: Import settings
 from django.conf.urls.static import static  # <-- NEW: Import static
+from django.contrib.sitemaps.views import sitemap
+from services.sitemaps import StaticViewSitemap, ServiceSitemap
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("services.urls")),
+     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
     
 ]
 
@@ -17,3 +20,8 @@ if settings.DEBUG:  # <-- NEW: Add this if block
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     # Optional: Serve static files in development (though React dev server handles most of that)
     # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+sitemaps = {
+    'static': StaticViewSitemap,
+    'services': ServiceSitemap,
+}
